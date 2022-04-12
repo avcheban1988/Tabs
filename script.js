dd129586b29553cf548fd1421476a6a32811662b
avcheban1988 <avcheban@gmail.com>
2022-04-12 20:50:52 +0300

    3 step

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
-
+    
+    
+    
+    
+//tabs
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabcontent = document.querySelectorAll('.info-tabcontent');
@@ -33,9 +37,56 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     })
-})
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+//timer
+let deadLine = '2023-03-26';
+
+function getTimeRamaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor(t/(1000*60*60));
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
 }
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+        if (getTimeRamaining(endtime).total <= 0) {  //проверка на то, что время вышло, чтоб назад не шло
+            clearInterval(timeInterval);
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+        }
+    
+    function updateClock(){
+        let t = getTimeRamaining(endtime);
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+        if (t.total < 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+setClock('timer', deadLine)
+
+//modalWindow
+
+let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+
+
+    
+});
+
